fcecc66d4ac146933f4b3a073a0a9c05512129a9
cirocki <marcincirocki89@gmail.com>
2025-03-08 21:30:29 +0100

    Add styles to video player

--- src/app/components/atoms/sanityVideo/SanityVideo.tsx
+++ src/app/components/atoms/sanityVideo/SanityVideo.tsx
@@ -1,18 +1,19 @@
 "use client";
 import ReactPlayer from "react-player";
-
+import styles from "./video-styles.module.scss";
 interface SanityVideoProps {
   url: string;
 }
 
 export default function SanityVideo({ url }: SanityVideoProps) {
   return (
-    <div className="player-wrapper">
+    <div className={styles.player_wrapper}>
       <ReactPlayer
-        className="react-player"
+        className={styles.react_player}
         url={url}
         width="100%"
         height="100%"
+        controls={true}
       />
     </div>
   );
